d0f7f092640b8b193efb097d01d91e1906496eea
Chanda Hubbard <chanda.hubbard@gmail.com>
2020-06-26 11:06:44 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    PORT: 8080,
-    NODE_ENV: 'development',
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
     DB_URL: process.env.DB_URL || 'postgres://ymzzpjmz:kUdfw2oErRwCaXcuLHqSq0mBimn9DRmm@hanno.db.elephantsql.com:5432/ymzzpjmz',
     TEST_DATABASE_URL: 'postgres://ymzzpjmz:kUdfw2oErRwCaXcuLHqSq0mBimn9DRmm@hanno.db.elephantsql.com:5432/ymzzpjmz',
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/"
