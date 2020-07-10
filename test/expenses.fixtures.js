629f32a4f8cb825d354cf58289c6419c0fddf79e
Chanda Hubbard <chanda.hubbard@gmail.com>
2020-07-10 11:36:37 -0700

    remove expense-services.spec update TEST_DB_URL in config.js add GET test to expenses-endpoints.spec.js

--- test/expenses.fixtures.js
+++ test/expenses.fixtures.js
@@ -1,36 +1,28 @@
 function makeExpensesArray() {
   return [
     {
-      id: 1,
       name: "first expense",
       amount: "100.00",
       type_id: "14",
-      category: "Recurring",
-      date: "2029-01-22T16:28:32.615Z",
+      category: "Recurring"
     },
       {
-        id: 2,
         name: "expense 2",
         amount: "20.00",
         type_id: "13",
-        category: "Discretionary",
-        date: new Date("2029-01-22T16:28:32.615Z"),
+        category: "Discretionary"
       },
       {
-        id: 3,
         name: "expense 3",
         amount: "30.00",
         type_id: "5",
-        category: "Savings/Investments",
-        date: new Date("2029-01-22T16:28:32.615Z"),
+        category: "Savings/Investments"
       },
       {
-        id: 4,
         name: "fourth expense",
         amount: "10.00",
         type_id: "12",
-        category: "Recurring",
-        date: new Date("2029-01-22T16:28:32.615Z"),
+        category: "Recurring"
       },
   ];
 }
